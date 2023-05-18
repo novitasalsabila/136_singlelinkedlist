821a3af32e45678f57ec657122aec7c70a09e9b2
novitasalsabila <novnovv10@gmail.com>
2023-05-18 23:10:44 +0700

    Memeriksa apakah node dengan NIM yang ingin dihapus ada dalam linked list. Pemeriksaan dilakukan dengan memanggil fungsi searchNode(nim, previous, current). Jika tidak ditemukan, fungsi deleteNode() akan mengembalikan false.

--- 136_singlelinkedlist/136_singlelinkedlist.cpp
+++ 136_singlelinkedlist/136_singlelinkedlist.cpp
@@ -69,4 +69,7 @@ bool serachNode(int nim, Node* current, Node* previous) {
 bool deleteNode(int nim) {
 	Node* current = START;
 	Node* previous = START;
-	
+	if (serachNode(nim, previous, current) == false)
+		return false;
+}
+
