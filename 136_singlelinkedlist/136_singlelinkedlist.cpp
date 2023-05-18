a26b5295370829885600453831ad424a09c1d513
novitasalsabila <novnovv10@gmail.com>
2023-05-18 23:21:29 +0700

    Membuat sebuah menu pilihan menggunakan switch case.

--- 136_singlelinkedlist/136_singlelinkedlist.cpp
+++ 136_singlelinkedlist/136_singlelinkedlist.cpp
@@ -132,4 +132,46 @@ int main() {
 			cout << "5. Keluar" << endl;
 			cout << "pilihan:";
 			cin >> pilihan;
-			
+			switch (pilihan)
+			{
+			case 1:
+				addNode();
+				cout << "Data Berhasil Ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List Kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+
+				int nim;
+				cout << "Masukan NIM:";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim" << nim << "berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "Data tidak ditemukan" << endl;
+				break;
+			case 3:
+				traverse();
+				break;
+			case 4:
+				searchData();
+				break;
+			case 5:
+				break;
+			default:
+				cout << "Pilihan tidak ada" << endl;
+				break;
+
+			}
+		}
+		
